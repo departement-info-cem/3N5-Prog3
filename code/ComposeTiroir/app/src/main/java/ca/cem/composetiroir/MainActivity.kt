eb67e8a179c07c55c242b5c84edca4009e927293
Joris Deguet <joris.deguet@gmail.com>
2025-06-07 14:20:28 -0400

    cleanup tests

--- code/ComposeTiroir/app/src/main/java/ca/cem/composetiroir/MainActivity.kt
+++ code/ComposeTiroir/app/src/main/java/ca/cem/composetiroir/MainActivity.kt
@@ -6,11 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import ca.cem.composetiroir.ui.theme.ComposeTiroirTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +24,49 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeTiroirTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Navigation()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeTiroirTheme {
-        Greeting("Android")
+fun Navigation(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = "connexion",
+        //modifier = modifier.padding(padding)
+    ) {
+        composable(route = "connexion") { Connexion(navController, modifier) }
+        composable(route = "ajout") { Ajout(navController, modifier) }
+        composable(route = "accueil") { Accueil(navController,modifier) }
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Connexion(
+    navController: NavHostController,
+    modifier: Modifier) {
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Ajout(
+    navController: NavHostController,
+    modifier: Modifier) {
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Accueil(
+    navController: NavHostController,
+    modifier: Modifier) {
+
+}
+
