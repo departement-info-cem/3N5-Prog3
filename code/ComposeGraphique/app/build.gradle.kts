01f4d222aecbb3d12dc260058e65b950fd35cb7a
Joris Deguet <joris.deguet@gmail.com>
2025-10-15 13:45:26 -0400

    Downgrade

--- code/ComposeGraphique/app/build.gradle.kts
+++ code/ComposeGraphique/app/build.gradle.kts
@@ -6,14 +6,12 @@ plugins {
 
 android {
     namespace = "ca.cem.composegraphique"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "ca.cem.composegraphique"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- code/ComposeGraphique/app/src/main/java/ca/cem/composegraphique/MainActivity.kt
+++ code/ComposeGraphique/app/src/main/java/ca/cem/composegraphique/MainActivity.kt
@@ -11,10 +11,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import ca.cem.composegraphique.ui.theme.ComposeGraphiqueTheme
